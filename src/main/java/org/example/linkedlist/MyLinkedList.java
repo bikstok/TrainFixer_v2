ab2890ebd7f522553821645a99a5aca315229bf9
bikstok <jkjaerulff@gmail.com>
2025-09-16 12:57:17 +0200

    Added tail to my linked list to get time complixity in 0(1) instead of 0(n)

--- src/main/java/org/example/linkedlist/MyLinkedList.java
+++ src/main/java/org/example/linkedlist/MyLinkedList.java
@@ -5,25 +5,80 @@ import java.util.NoSuchElementException;
 
 public class MyLinkedList<T> implements Iterable<T> {
     private Node<T> head;
+    private Node<T> tail;
     private int size;
 
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // Tilføj element bagerst i listen
     public void add(T element) {
         Node<T> newNode = new Node<>(element);
         if (head == null) {
             head = newNode;
+            tail = newNode;
         } else {
-            Node<T> current = head;
-            while (current.next != null) current = current.next;
-            current.next = newNode;
+            tail.next = newNode;
+            tail = newNode;
         }
         size++;
     }
 
+    // Tilføj element forrest i listen (valgfri, men nyttig)
+    public void addFirst(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    // Hent element på en bestemt position
     public T get(int index) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
         Node<T> current = head;
         for (int i = 0; i < index; i++) current = current.next;
         return current.data;
+    }
+
+    // Fjern første element
+    public T removeFirst() {
+        if (head == null) throw new NoSuchElementException();
+        T data = head.data;
+        head = head.next;
+        if (head == null) {
+            tail = null; // listen er nu tom
+        }
+        size--;
+        return data;
+    }
+
+    // Fjern sidste element
+    public T removeLast() {
+        if (head == null) throw new NoSuchElementException();
+        if (head == tail) { // kun ét element
+            T data = head.data;
+            head = null;
+            tail = null;
+            size = 0;
+            return data;
+        }
+        Node<T> current = head;
+        while (current.next != tail) {
+            current = current.next;
+        }
+        T data = tail.data;
+        tail = current;
+        tail.next = null;
+        size--;
+        return data;
     }
 
     public int size() {
@@ -32,6 +87,14 @@ public class MyLinkedList<T> implements Iterable<T> {
 
     public Node<T> getHead() {
         return head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     @Override
